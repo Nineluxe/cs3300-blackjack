da9b66d14d2f41cdc096b8ba54a531871366ed1b
eboyd3 <eboyd3@uccs.edu>
2025-10-30 18:12:31 -0600

    Correct scoring and comments
    Added a function to get a cards correct scoring value, as well as added comments and a test instance of Card

--- command line blackjack.py
+++ command line blackjack.py
@@ -1,17 +1,44 @@
 import random
 
 class Card:
+    #String array of suit names
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    #String array of names of cards for displaying
     names = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+
     def __init__(self, value, suitNum):
         self.value = value
         self.suit = Card.suits[suitNum]
-    
+        #Have to make a seperate function to get the cards actual value for the game, 
+        #since face cards and aces value don't match their index in the array unlike numbered cards
+        self.scoreValue = self.generateScoreValue()
+
     def getValue(self):
         return self.value
     
     def getSuit(self):
         return self.suit
     
+    #Gets the actual scoring value for the card
+    #Note: Will likely need to add something to this for aces later, 
+    #since their value can change to 1 if 11 would put the total score over 21
+    #could handle that outside of the class though
+    def generateScoreValue(self):
+        if(self.value <= 10): #Numbered cards
+            num = self.value
+        elif(self.value in range(11, 14)): #Face cards
+            num = 10
+        elif(self.value == 14): #Aces
+            num = 11 
+        return num
+    
+    def getScoreValue(self):
+        return self.scoreValue
+    
+    #Prints the cards name and value in the form of "Ace of Hearts"
     def nicePrint(self):
-        print(Card.name[self.value -1] + " of " + self.suit)
+        print(Card.names[self.value -1] + " of " + self.suit)
+
+testCard = Card(random.randint(1, 14), random.randint(0, 3))
+testCard.nicePrint()
+print(testCard.getScoreValue())
