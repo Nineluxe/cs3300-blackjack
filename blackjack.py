93d9461b809528d22baf62c12fc9f2033d0bd4b0
Bryce <64986044+Nineluxe@users.noreply.github.com>
2025-11-05 17:18:15 -0700

    WIP card drawing

--- blackjack.py
+++ blackjack.py
@@ -20,17 +20,21 @@ entityList = list()
 pygame.font.init()
 
 fontSize = 9
-font = pygame.font.Font("assets/fonts/dungeon-mode.ttf", fontSize)  # None = default system font, 24 = size
+cardFont = pygame.font.Font("assets/fonts/dungeon-mode.ttf", 9)  # None = default system font, 24 = size
+uiFont = pygame.font.Font("assets/fonts/monogram-extended.ttf", 16)
 mouse = pygame.mouse.get_pos()
 mouse_scaled = (mouse[0] / SCALE, mouse[1] / SCALE)
 debugging = True
 running = True
 game_paused = False
+drawables = list()
 
 # Card types
 cardFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 cardSuits = ['♠', '♣', '♥', '♦']
 cardBack = pygame.image.load("assets/pixelCardback.png")
+cardWidth = 60
+cardHeight = 100
 
 # Create surfaces
 base_surface = pygame.Surface( (BASE_WIDTH, BASE_HEIGHT) )
@@ -38,20 +42,28 @@ window = pygame.display.set_mode((BASE_WIDTH * SCALE, BASE_HEIGHT * SCALE))
 pygame.display.set_caption("Super Blackjack 9000")
 
 
+# Simple linear interpolation function
+def lerp(start, stop, amount):
+    return start + (stop - start) * amount
+
 ###################### CARD CLASS ####################
 class Card:
 
     ###### INITIALIZATION ######
     def __init__(self, x, y, faceText="A", suitChar="♥",
                  fillColor=WHITE, borderColor=BLACK, borderWidth=2, cornerRadius=2):
-        
+
         # Functional properties
         self.score = self.getScore(faceText)
 
         # Drawing properties
-        self.isFaceUp = False
         self.x, self.y = x, y
-        self.w, self.h = 60, 100
+        self.desiredX, self.desiredY = x, y
+        self.w, self.h = cardWidth, cardHeight
+        self.shadowSurf = pygame.Surface((self.w, self.h), pygame.SRCALPHA)
+        self.doDrawShadow = True
+        self.shadowOffset = (6, 6)
+        self.isFaceUp = True
         self.faceText = faceText
         self.suitChar = suitChar
         self.fillColor = fillColor
@@ -72,6 +84,13 @@ class Card:
     def toString(self):
         return f"{self.faceText} of {self.suitChar} (Score: {self.score})"
     
+    ###### UPDATE METHOD ######
+    def update(self, dt):
+        if self.x != self.desiredX:
+            self.x = lerp(self.x, self.desiredX, dt * 10)
+        if self.y != self.desiredY:
+            self.y = lerp(self.y, self.desiredY, dt * 10)
+
     ###### DRAW METHOD ######
     def draw(self, surface):
 
@@ -91,67 +110,132 @@ class Card:
 
         # fill
         pygame.draw.rect(surface, self.fillColor, rect, border_radius=self.cornerRadius)
+
         # border
         pygame.draw.rect(surface, self.borderColor, rect, width=self.borderWidth, border_radius=self.cornerRadius)
 
         # corner pips (rank + suit)
         rankColor = RED if self.suitChar in ("♥", "♦") else BLACK
-        rankText = font.render(self.faceText, False, rankColor)
-        suitText = font.render(self.suitChar, False, rankColor)
+        rankText = cardFont.render(self.faceText, False, rankColor)
+        suitText = cardFont.render(self.suitChar, False, rankColor)
         surface.blit(rankText, (self.x + 8, self.y + 6))
         surface.blit(suitText, (self.x + 8, self.y + 8 + rankText.get_height()))
 
         # big center suit
-        centerSuit = font.render(self.suitChar, False, rankColor)
+        centerSuit = cardFont.render(self.suitChar, False, rankColor)
         surface.blit(centerSuit, centerSuit.get_rect(center=rect.center))
+
+
+###################### GAME CLASS ######################
+class Game:    
+
+    def __init__(self):
+        self.x = BASE_WIDTH - (cardWidth + 20)
+        self.y = BASE_HEIGHT + (cardHeight + 20)
+        self.deck = []
+        self.initializeDeck()
+
+        # Add deck to drawables for drawing
+        drawables.append(self)
+
+    # Draw back of card to represent deck
+    def draw(self, surface):
+        if len(self.deck) > 0:
+            surface.blit(cardBack, (self.x, self.y))
+
+    # Initializes deck with 52 cards and shuffles them
+    def initializeDeck(self):
+        for suit in cardSuits:
+            for face in cardFaces:
+                newCard = Card(0, 0, face, suit)
+                self.deck.append(newCard)
+        random.shuffle(self.deck)
+
 
 ###################### PLAYER CLASS ####################
 class Player:
 
-    def __init__(self, name):
+    def __init__(self, name, handPosition=[0,0]):
         self.name = name
         self.hand = []
         self.score = 0
+        self.handPosition = handPosition
     
+    # Add a card to the player's hand
     def addCard(self, card):
+
+        # Add card to the relevant lists
         self.hand.append(card)
+        drawables.append(card)
+
+        # Update all the cards desired positions
+        totalCards = len(self.hand)
+        for index, card in enumerate(self.hand):
+
+            cardIndex = index - (totalCards - 1) / 2
+
+            # Update desired positions of cards hand
+            card.desiredX = self.handPosition[0] + (cardIndex * (cardWidth + 10))
+            card.desiredY = self.handPosition[1]
+
+        # Update your hand score
         self.updateScore()
 
     def updateScore(self):
         self.score = sum(card.score for card in self.hand)
+
+    def drawScore(self, surface, x, y):
+        scoreText = uiFont.render(f"{self.name} Score: {self.score}", True, WHITE)
+        surface.blit(scoreText, (x, y))
 
     def destroyCard(self, card):
         if card in self.hand:
             self.hand.remove(card)
             self.updateScore()  
 
+
 ###################### HAND ####################
-user = Player("User")
-dealer = Player("Dealer")
+userStartingHandPosition = [
+    (BASE_WIDTH // 2) - (cardWidth // 2), 
+    BASE_HEIGHT - (cardHeight + 20)
+]
+
+game = Game()
+user = Player("User", userStartingHandPosition)
+dealer = Player("Dealer", handPosition=(BASE_WIDTH // 2, cardHeight + 20))
 
 # Creates a card object and returns it
-def createCard(x, y, faceText, suitChar):
-    return Card(x, y, faceText, suitChar)
+def drawDeckCard(x, y):
+    newCard = game.deck.pop()
+    newCard.x = x
+    newCard.y = y
+    return newCard
 
 ###################### CONTROLS ####################
-
 def processControls():
-    
+
+    global user, dealer
+
     for event in pygame.event.get():
 
         # Controls: Change size
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 fontSize += 1
-                font = pygame.font.Font("assets/fonts/dungeon-mode-inverted.ttf", fontSize)
+                uiFont = pygame.font.Font("assets/fonts/monogram-extended.ttf", fontSize)
                 print("Font size:", fontSize)
             elif event.key == pygame.K_DOWN:
                 fontSize = max(1, fontSize - 1)
-                font = pygame.font.Font("assets/fonts/dungeon-mode-inverted.ttf", fontSize)
+                uiFont = pygame.font.Font("assets/fonts/monogram-extended.ttf", fontSize)
                 print("Font size:", fontSize)
-            
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+            # Debug: Add card to user hand
             if event.key == pygame.K_0:
-                user.addCard(createCard(50, 50, 'A', '♠'))
+                xPos = user.handPosition[0]
+                yPos = user.handPosition[1]
+                user.addCard(drawDeckCard(xPos, yPos))
 
         # Debug: Quit game
         if event.type == pygame.QUIT:
@@ -159,13 +243,9 @@ def processControls():
             running = False
             exit()
 
-    # Get mouse position and scale it based on window size
-    mouse = pygame.mouse.get_pos()
-    mouse_scaled = (mouse[0] / SCALE, mouse[1] / SCALE)
 
 
 #################### MAIN LOOP ####################
-
 while running:
     
     # Handle controls
@@ -174,14 +254,24 @@ while running:
     # Draw background
     base_surface.fill( (29, 71, 46) )
 
-    # Draw cards
+    # Draw every object in drawables list
+    for obj in drawables:
+        obj.draw(base_surface)
+
+    # Use update method to move cards toward desired positions
     for card in user.hand:
-        card.draw(base_surface)
-
+        card.update(clock.get_time() / 1000.0)
     for card in dealer.hand:
-        card.draw(base_surface)
+        card.update(clock.get_time() / 1000.0)
 
-    surface.draw
+    # Draw player score
+    user.drawScore(base_surface, 10, BASE_HEIGHT - 30)
+    dealer.drawScore(base_surface, BASE_WIDTH - 150, 10)
+
+    # Get mouse position and scale it based on window size
+    mouse = pygame.mouse.get_pos()
+    mouse_scaled = (mouse[0] / SCALE, mouse[1] / SCALE)
+
     # Scale and blit to window
     scaled_surface = pygame.transform.scale(base_surface, (WINDOW_WIDTH, WINDOW_HEIGHT))
     window.blit(scaled_surface, (0, 0))
